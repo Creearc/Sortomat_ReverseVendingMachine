3109f6159f8913589c5099be6ca5bb3b9da132a7
Alexandr <knxandr@rambler.ru>
2021-07-15 01:12:09 +0500

    1

---
+++ components/us_sensors.py
@@ -0,0 +1,121 @@
+# Модуль ультразвуковых датчиков
+import sys
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+
+class US_sensor:
+  def __init__(self, TRIGGER_PIN, ECHO_PIN, measures_count):
+    # Пины
+    self.TRIGGER_PIN = TRIGGER_PIN
+    self.ECHO_PIN = ECHO_PIN
+    GPIO.setup(self.TRIGGER_PIN, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(self.ECHO_PIN, GPIO.IN)
+    
+    # Переменные
+    self.measures_count = measures_count
+    self.distance = [11.0 for i in range(measures_count)]
+
+  # Измерение расстояния при помощи УЗ датчика
+  def measure(self):
+    t = time.time()
+    pulse_end_time, pulse_start_time = t, t
+    GPIO.output(self.TRIGGER_PIN, GPIO.HIGH)
+    time.sleep(0.00001)
+    GPIO.output(self.TRIGGER_PIN, GPIO.LOW)
+
+    while GPIO.input(self.ECHO_PIN)==0:
+      pulse_start_time = time.time()
+      if time.time() - t > 0.2:
+        print('Не удалось измерить расстояние')
+        return 11.0
+    while GPIO.input(self.ECHO_PIN)==1:
+      pulse_end_time = time.time()
+      if time.time() - t > 0.2:
+        print('Не удалось измерить расстояние')
+        return 11.0
+
+    pulse_duration = pulse_end_time - pulse_start_time
+    distance = round(pulse_duration * 17150, 2)
+    time.sleep(0.1)
+    return distance
+
+  def check_measure(self, d, k=0.1):
+    for i in range(self.measures_count - 1, 0, -1):
+      self.distance[i] = self.distance[i - 1]
+    self.distance[0] = d
+      
+    
+# Отсек приема
+class US_sensor_Hand(US_sensor): 
+  def __init__(self, TRIGGER_PIN=18, ECHO_PIN=14,
+               HAND_DISTANCE=10, HAND_TIME_LIMIT=5.0,
+               measures_count=2):
+    super().__init__(TRIGGER_PIN, ECHO_PIN, measures_count)
+    
+    self.HAND_DISTANCE = HAND_DISTANCE
+    self.HAND_TIME_LIMIT = HAND_TIME_LIMIT
+
+    self.hand_state = False
+   
+  # Обнаружение руки
+  def isHand(self):
+    self.check_measure(self.measure())
+    h = True
+    nh = True
+    for i in self.distance:
+      if i > self.HAND_DISTANCE:
+        h = False
+      else:
+        nh = False
+    if h:
+      self.hand_state = True
+    if nh:
+      self.hand_state = False
+          
+    if self.hand_state:
+      return True
+    else:
+      return False
+
+# Отсек приема
+class US_sensor_Storage(US_sensor): 
+  def __init__(self, TRIGGER_PIN=7, ECHO_PIN=15,
+               FULL_DISTANCE=20, 
+               measures_count=20):
+    super().__init__(TRIGGER_PIN, ECHO_PIN, measures_count)
+
+    self.FULL_DISTANCE = FULL_DISTANCE
+    self.storage_state = False
+
+  def isFull(self):
+    self.check_measure(self.measure())
+    f = True
+    nf = True
+    for i in self.distance:
+      if i > self.FULL_DISTANCE:
+        f = False
+      else:
+        nf = False
+    if f:
+      self.storage_state = True
+    if nf:
+      self.storage_state = False
+          
+    if self.storage_state:
+      return True
+    else:
+      return False
+
+
+if __name__ == '__main__':
+  try:
+    d1 = US_sensor_Storage()
+    while True:
+      print(d1.isFull())
+      print(d1.distance)
+  except KeyboardInterrupt:
+    GPIO.cleanup()
+    sys.exit()  
---
+++ main/main_stable_0.py
@@ -0,0 +1,315 @@
+# export DISPLAY=":0" && python3 main_0_2.py
+import os
+import sys
+import RPi.GPIO as GPIO
+from multiprocessing import Process, Value, Queue
+import cv2
+import threading
+import time
+
+import sys
+path = '/'.join(sys.path[0].replace('\\', '/').split('/')[:-1])
+sys.path.insert(0, path)
+
+from components import monitor
+from components import qr_gen
+from components import light
+from components import rotator
+from components import ir_sensors
+from components import us_sensors
+from components import destroyer
+from components import weight
+from components import camera
+from compomemts import client_app
+
+from components.neural_network import roi_function
+
+#import data_creation_main as dcm
+
+sys.path.insert(0, '{}/components/neural_network'.format(path))
+from model_component import Model
+
+model1 = Model("model_full_7classes_13may.tflite")
+model1.debug = False
+model1.input_shape = (512, 297, 3)
+model1.labels = ['al__Other', 'empty_Empty', 'hdpe__ChemWhitemilk',
+                 'Other__Other2', 'pet__Brown', 'pet__ChemOilMilk',
+                 'pet__Green', 'pet__Transparent']
+
+model2 = Model("model_roi_7classes_13may.tflite")
+model2.debug = False
+model2.input_shape = (448, 224, 3)
+model2.labels = ['al__Other', 'empty_Empty', 'hdpe__ChemWhitemilk',
+                 'Other__Other2', 'pet__Brown', 'pet__ChemOilMilk',
+                 'pet__Green', 'pet__Transparent']
+
+if not os.path.exists('data'):
+  os.mkdir('data')
+
+save_path = 'data/{}'.format(len(os.listdir('data')))
+os.mkdir(save_path)
+
+try:
+  machine_state = 0
+  machine_state_old = -1
+  state_changed = False
+
+  m = monitor.Monitor(1366, 768)
+  m.start()
+  m.state(0)
+
+  print("_______________________________________________________________")
+  print("Подготовка связи с космосом")
+  client = client_app.Client()
+  client.start()
+  print("Связь с космосом готова")
+
+  print("_______________________________________________________________")
+  print("Подготовка датчика веса")
+  w = weight.Weight()
+  print("Датчик веса готов")
+
+  print("_______________________________________________________________")
+  print("Подготовка камеры")
+  c = camera.Camera()
+  c.start()
+  print("Камера готова")
+
+  print("_______________________________________________________________")
+  print("Подготовка освещения")
+  l = light.Light()
+  l.color_preset('blue')
+  print("Освещение готово")
+
+  print("_______________________________________________________________")
+  print("Подготовка ИК датчиков")
+  ir = ir_sensors.IR_sensors()
+  print("ИК датчики готовы")
+
+  print("_______________________________________________________________")
+  print("Подготовка УЗ датчика")
+  us = us_sensors.US_sensor_Storage()
+  print("УЗ датчик готов")
+
+  print("_______________________________________________________________")
+  print("Подготовка крыльчатки")
+  r = rotator.Rotator()
+  r.calibrate()
+  print("Крыльчатка готова")
+
+  print("_______________________________________________________________")
+  print("Подготовка сминателя")
+  s = destroyer.Destroyer()
+  s.start()
+  print("Сминатель готов")
+
+  c_time = time.time()
+  points = 0
+  points_timeout = 0
+  POINTS_TIMEOUT_WAIT = 10
+  POINTS_TIMEOUT_QR = 15
+
+  while True:
+    if client.manual == True:
+      machine_state = client.state
+    state_changed = False
+    if machine_state != machine_state_old:
+      state_changed = True
+    machine_state_old = machine_state
+
+    # 0 Ожидание
+    if machine_state == 0:
+      if state_changed:
+        m.state(1)
+        l.color_preset('blue')
+        if points > 0:
+          points_timeout = time.time() + POINTS_TIMEOUT_WAIT
+
+      if points > 0:
+        t = int(points_timeout - time.time())
+        if t <= 0:
+          m.set_points(0, 0)
+          m.state(10)
+          qr_gen.generate(points, '{}/imgs/qrcode.png'.format(path))
+          points = -1
+          qr_gen.make_img('{}/imgs/qr.png'.format(path), '{}/imgs'.format(path),'{}/imgs/qrcode.png'.format(path))
+          points_timeout = time.time() + POINTS_TIMEOUT_QR
+        else:
+          m.state(9)
+        m.set_points(points, t)
+      elif points < 0:
+        t = int(points_timeout - time.time())
+        if t < 0:
+          m.state(1)
+          points = 0
+          points_timeout = 0
+        else:
+          m.state(8)
+        m.set_points(points, t)
+            
+      if time.time() - c_time > 1.5:
+        img = c.get_img()    
+        if camera.is_object_blue(img, show=False, debug=False):
+          machine_state = -2
+        else:
+          machine_state = 0
+          w.set_null()
+        c_time = time.time()
+      if ir.hand():
+        m.set_points(0, 0)
+        machine_state = 1
+
+
+    # -1 В отсеке лежит объект, который нужно забрать
+    elif machine_state == -1:
+      if state_changed:
+        m.state(4)
+        l.color_preset('red')
+      img = c.get_img()      
+      if not camera.is_object_red(img, show=False, debug=False):
+        machine_state = 0
+      if ir.hand():
+        machine_state = 1
+            
+    # -2 В отсеке лежит объект, который нужно забрать
+    elif machine_state == -2:
+      if state_changed:
+        m.state(5)
+        l.color_preset('red')
+      img = c.get_img()  
+      if not camera.is_object_red(img, show=False, debug=False):
+        machine_state = 0
+      if ir.hand():
+        machine_state = 1
+
+    elif machine_state == -3:
+      if state_changed:
+        m.state(7)
+        l.color_preset('red')
+      img = c.get_img()      
+      if not camera.is_object_red(img, show=False, debug=False):
+        machine_state = 0
+      if ir.hand():
+        machine_state = 1
+
+    # 1 В отсеке приема есть рука
+    elif machine_state == 1:
+      if state_changed:
+        l.color_preset('blue')
+        m.state(2)
+      if not ir.hand():
+        machine_state = 2
+
+
+    # 2 В отсеке приема может находиться объект
+    elif machine_state == 2:
+      if ir.hand():
+        machine_state = 1
+      time.sleep(0.2)
+      img = c.get_img()      
+      if camera.is_object_blue(img, show=False, debug=False):
+        machine_state = 3
+      else:
+        machine_state = 0
+
+    # 3 В отсеке лежит объект
+    elif machine_state == 3:
+      if w.is_heavy():
+        machine_state = -3
+      else:
+        machine_state = 4
+
+
+    # 4 Ожидание ответа от распознавания
+    elif machine_state == 4:
+      if ir.hand():
+        machine_state = 1
+      else:
+        l.color_preset('white', 100)
+        time.sleep(0.2)
+        for i in range(5):
+          img = c.get_img()
+        out = roi_function.roi(img)
+        print(out.shape)
+        if out.shape == (0, 0, 3):
+          out = img
+
+        result_1 = model1.classify_images([img[150:610, 80:1020]])
+
+        result_roi_1 = model1.classify_images([out])
+        result_roi_2 = model2.classify_images([out])
+
+        results = [result_1, result_roi_1, result_roi_2]
+        print(results)
+        special = 'al__Other'
+        
+        if 'Other__Other2' in results or 'empty_Empty' in results or (results.count(special) > 0 and results.count(special) < 3):
+          ai_answer = 1
+          
+        else:
+          ai_answer = 0
+          if results.count(special) == 3:
+            points += 1
+            r.left = False
+            s.use = False
+          else:
+            points += 1
+            r.left = True
+            s.use = True
+
+        cv2.imwrite('{}/{}.png'.format(save_path, len(os.listdir(save_path)), ' '.join(results)), img)
+        
+        if ai_answer == 0:
+          r.start()
+          m.state(3)
+          machine_state = 5
+        else:
+          m.state(4)
+          machine_state = -1
+
+
+    # 5 Вращение крыльчатки
+    elif machine_state == 5:
+      if state_changed:
+        m.state(3) 
+        l.color_preset('green')
+      if not r.working:
+        machine_state = 7
+      if ir.hand():
+        r.stop()
+        machine_state = 6
+      
+
+    # 6 Остановка крыльчатки
+    elif machine_state == 6:
+      if state_changed:
+        m.state(2) 
+        l.color_preset('red')
+      if not ir.hand():
+        time.sleep(1.0)
+        r.calibrate()
+        machine_state = 5
+
+
+    # 7 Включение сминателя
+    elif machine_state == 7:
+      if s.use == True:
+        s.launch_destroyer()
+        c_time = time.time()
+      machine_state = 0
+
+    if machine_state != machine_state_old:
+      print("Состояние сортомата: {} ".format(machine_state))
+      print("ИК датчики {} ".format(str(ir.hand())))
+      print("Крыльчатка: {}".format(r.state))
+      print("_______________________________________________________________")
+    time.sleep(0.1)
+      
+
+
+except KeyboardInterrupt:
+  s.stop_destroyer()
+  GPIO.cleanup()
+  print("_______________________________________________________________")
+  print("Завершение работы")
+  sys.exit()
