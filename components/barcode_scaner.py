250ebb428167825757ccedf82cf1ff83a9f403e6
Alexandr <knxandr@rambler.ru>
2021-11-06 20:46:12 +0500

    Update barcode_scaner.py

--- components/barcode_scaner.py
+++ components/barcode_scaner.py
@@ -6,14 +6,12 @@ class Scaner():
   def __init__(self, device='/dev/hidraw0', bandwidth=9600):
     self.ser = serial.Serial(device, bandwidth)
 
-  def read(self):
-    s = b''
-    while s == b'':
-      s = self.ser.readline()
-    return int(s.decode())
+def read():
+  out = ''
+  while out == '':
+    out = os.popen("cat /dev/serial/by-id/usb-USBKey_Chip_USBKey_Module_202730041341-if00").read()
+  return out
 
-  def tell(self, command):
-    self.ser.write('{}\n'.format(command).encode())
 
 
 
@@ -21,6 +19,6 @@ if __name__ == '__main__':
   scaner = Scaner()
   print('Reading')
   while True:
-    print(scaner.read())
+    print(read())
 ##   while True:
 ##     print(keyboard.read_key())
