44ae068a6dbdbcaabcae790f418d025ad3c84b83
Alexandr <knxandr@rambler.ru>
2021-11-07 01:03:18 +0500

    up

--- components/barcode_scaner.py
+++ components/barcode_scaner.py
@@ -1,31 +1,29 @@
-import serial
-import keyboard
-import os
-import time
-
-
-##class Scaner():
-##  def __init__(self):
-    
-      
-
 def read():
-  with open("/dev/serial/by-id/usb-USBKey_Chip_USBKey_Module_202730041341-if00", 'r') as f:
+  out = ''
+  last = 0
+  counter = 0 
+  with open('/dev/usb/hiddev0', 'rb') as f:
     while True:
+      tmp = f.read(8)
       try:
-        out = f.readline()
-        print('? ', out)
+        tmp = tmp[3:].decode()[1]
+        out = '{}{}'.format(out, tmp)
       except:
-        print('________')
+        print('[BARCODE_SCANER] Unsupported code')
+        return None
+  
+      if ord(tmp) == 125 and last == 34:
         break
-    return out
+      else:
+        last = ord(tmp)
+      counter += 1
+    return out.split('data')[-1][3:-2]
+  
 
 
 
 
 if __name__ == '__main__':
-  #scaner = Scaner()
-  time.sleep(2.0)
   print('Reading')
   while True:
     print(read())
