842d069bfe32b573ed2af2fc5e6603b68458c097
Alexandr <knxandr@rambler.ru>
2021-11-08 02:02:13 +0500

    Update main_thread_0.py

--- main/main_thread_0.py
+++ main/main_thread_0.py
@@ -8,15 +8,18 @@ from config import config
 
 print('[MAIN_THREAD] Компоненты готовы')
 
+components = config.components
+
 data = dict()
-data['state']
-data['old_state']
-data['next_state']
-data['state_changed']
-data['check_time']
-data['false_things_counter']
-data['points']
-data['add_points']
-data['hand_detection_time']
-data['save_path']
-data['user_id']
+data['state'] = 0 
+data['old_state'] = 0 
+data['next_state'] = 0 
+data['state_changed'] = False
+data['check_time'] = 0 
+data['false_things_counter'] = 0 
+data['points'] = 0 
+data['add_points'] = 0 
+data['hand_detection_time'] = 0 
+data['save_path'] = '{}/{}'.format(components['SAVE_PATH'],
+                                   len(os.listdir(components['SAVE_PATH'])))
+data['user_id'] = 0
