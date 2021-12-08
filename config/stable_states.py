9f45254f1c7c46d17bff05dab88622b57b118627
Creearc <knxandr@rambler.ru>
2021-12-08 18:07:56 +0500

    Update stable_states.py

--- config/stable_states.py
+++ config/stable_states.py
@@ -19,7 +19,7 @@ def state_0(components, data):
     components['weight'].set_null()
     data['check_time'] = time.time()
     components['rotator'].calibrate()
-    time.sleep(0.05)
+    time.sleep(0.1)
     print('TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT')
   if time.time() - data['check_time'] > components['ROTATOR_CALIBRATION_TIME']:
     return 0, 4
