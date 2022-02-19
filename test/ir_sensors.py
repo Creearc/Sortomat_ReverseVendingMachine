4f5bdb4b6eb6c7f9b81fe1c0e8cb27f0736ca945
Creearc <knxandr@rambler.ru>
2022-02-19 10:09:06 +0500

    Create ir_sensors.py

---
+++ test/ir_sensors.py
@@ -0,0 +1,61 @@
+# Модуль инфракрасных датчиков
+import sys
+import RPi.GPIO as GPIO
+import threading
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+class IR_sensors:
+  def __init__(self, PIN_1=31, PIN_2=32):
+    # Пины
+    self.PIN_1 = PIN_1
+    GPIO.setup(self.PIN_1, GPIO.IN)
+    GPIO.add_event_detect(self.PIN_1, GPIO.FALLING, callback=self.detect)
+
+    self.PIN_2 = PIN_2
+    GPIO.setup(self.PIN_2, GPIO.IN)
+    GPIO.add_event_detect(self.PIN_2, GPIO.FALLING, callback=self.detect)
+
+
+    # Переменные
+    self.motion = False
+    self.motion_delay = 1
+    self.motion_time = time.time()
+
+    self.lock = threading.Lock()
+
+  def start(self):
+    thrd = threading.Thread(target=self.process, args=())
+    thrd.start()
+
+  def process(self):
+    while True:
+      time.sleep(self.motion_delay)
+      if time.time() - self.motion_time > self.motion_delay:
+        with self.lock:
+          self.motion = False
+
+
+  # Изменение состояния датчика
+  def detect(self, channel):
+    self.motion = True
+
+  def hand(self):
+    return any([GPIO.input(self.PIN_1), GPIO.input(self.PIN_2)])
+
+  def show_all(self):
+    return [GPIO.input(self.PIN_1), GPIO.input(self.PIN_2)]
+
+
+if __name__ == '__main__':
+  try:
+    ir = IR_sensors()
+    while True:
+      print(ir.hand())
+      print(ir.show_all())
+      time.sleep(0.1)
+  except KeyboardInterrupt:
+    GPIO.cleanup()
+    sys.exit()
+
